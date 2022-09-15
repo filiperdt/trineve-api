a4e900e2b14e5267e36f9f393de099b7db58b59d
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2022-09-14 21:51:47 -0300

    Configuracao inicial do projeto

---
+++ src/main/java/br/com/trineveapi/trineveapi/TrineveapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.trineveapi.trineveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrineveapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrineveapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/trineveapi/trineveapi/TrineveapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trineveapi.trineveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrineveapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
